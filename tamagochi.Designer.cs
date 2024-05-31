2142687713103b412bb9091c10d2eca97a66fbf6
catexplodes <153719096+catexplodes@users.noreply.github.com>
2024-06-01 01:56:17 +0300

    Add project files.

---
+++ tamagochi.Designer.cs
@@ -0,0 +1,235 @@
+﻿namespace kursach
+{
+    partial class tamagochi
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(tamagochi));
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.button4 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.progressBar3 = new System.Windows.Forms.ProgressBar();
+            this.progressBar2 = new System.Windows.Forms.ProgressBar();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.button5 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // imageList1
+            // 
+            this.imageList1.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageList1.ImageStream")));
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            this.imageList1.Images.SetKeyName(0, "Custom-Edited---Tamagotchi-Customs---Lovelitchi_01.png");
+            this.imageList1.Images.SetKeyName(1, "Custom-Edited---Tamagotchi-Customs---Lovelitchi_02.png");
+            this.imageList1.Images.SetKeyName(2, "Custom-Edited---Tamagotchi-Customs---Lovelitchi_09.png");
+            this.imageList1.Images.SetKeyName(3, "Custom-Edited---Tamagotchi-Customs---Lovelitchi_10.png");
+            this.imageList1.Images.SetKeyName(4, "Custom-Edited---Tamagotchi-Customs---Lovelitchi_15.png");
+            this.imageList1.Images.SetKeyName(5, "Custom-Edited---Tamagotchi-Customs---Lovelitchi_04.png");
+            this.imageList1.Images.SetKeyName(6, "Custom-Edited---Tamagotchi-Customs---Lovelitchi_18.png");
+            this.imageList1.Images.SetKeyName(7, "Custom-Edited---Tamagotchi-Customs---Lovelitchi_19.png");
+            this.imageList1.Images.SetKeyName(8, "Custom-Edited---Tamagotchi-Customs---Lovelitchi_14.png");
+            this.imageList1.Images.SetKeyName(9, "Untitled-1.png");
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(489, 62);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(178, 179);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(77, 62);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(385, 106);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "кормить";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(77, 359);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(385, 106);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "спать";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(77, 206);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(385, 106);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "играть";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 10;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(77, 506);
+            this.button4.Name = "button4";
+            this.button4.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.button4.Size = new System.Drawing.Size(385, 106);
+            this.button4.TabIndex = 11;
+            this.button4.Text = "разбудить";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(639, 267);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(90, 25);
+            this.label3.TabIndex = 23;
+            this.label3.Text = "счастье";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(639, 160);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(112, 25);
+            this.label2.TabIndex = 22;
+            this.label2.Text = "усталость";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(634, 66);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(68, 25);
+            this.label1.TabIndex = 21;
+            this.label1.Text = "голод";
+            // 
+            // progressBar3
+            // 
+            this.progressBar3.Location = new System.Drawing.Point(639, 295);
+            this.progressBar3.Maximum = 10000;
+            this.progressBar3.Name = "progressBar3";
+            this.progressBar3.Size = new System.Drawing.Size(284, 41);
+            this.progressBar3.TabIndex = 20;
+            this.progressBar3.Value = 10000;
+            // 
+            // progressBar2
+            // 
+            this.progressBar2.Location = new System.Drawing.Point(639, 191);
+            this.progressBar2.Maximum = 10000;
+            this.progressBar2.Name = "progressBar2";
+            this.progressBar2.Size = new System.Drawing.Size(284, 41);
+            this.progressBar2.TabIndex = 19;
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(639, 94);
+            this.progressBar1.Maximum = 10000;
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(284, 41);
+            this.progressBar1.TabIndex = 18;
+            // 
+            // timer2
+            // 
+            this.timer2.Enabled = true;
+            this.timer2.Interval = 1000;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(639, 377);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(284, 56);
+            this.button5.TabIndex = 24;
+            this.button5.Text = "начать игру заново";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // tamagochi
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1054, 683);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.progressBar3);
+            this.Controls.Add(this.progressBar2);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "tamagochi";
+            this.Text = "ВВИ (Ваш Виртуальный Питомец)";
+            this.Load += new System.EventHandler(this.tamagochi_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ImageList imageList1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ProgressBar progressBar3;
+        private System.Windows.Forms.ProgressBar progressBar2;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.Button button5;
+    }
+}
+
---
+++ tamagochi.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace kursach
+{
+    public partial class tamagochi : Form
+    {
+        int sleep = 0;
+        int life = 1;
+
+        public tamagochi()
+        {
+            InitializeComponent();
+            pictureBox1.Image = imageList1.Images[0]; //начальный спрайт
+        }
+
+        private void button1_Click(object sender, EventArgs e) //кормить
+        { 
+            if(life!=0){
+                sleep = 0;
+                pictureBox1.Image = imageList1.Images[1];
+                if (progressBar1.Value > progressBar1.Minimum + 200)
+                    progressBar1.Value -= 200;
+                else progressBar1.Value = 0;
+            }
+
+        }
+
+        private void button2_Click(object sender, EventArgs e) //спать
+        {
+            if (life != 0)
+            {
+                pictureBox1.Image = imageList1.Images[3];
+                sleep = 1;
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e) //играть
+        {
+            if (life != 0)
+            {
+                sleep = 0;
+                pictureBox1.Image = imageList1.Images[5];
+                if (progressBar3.Value < progressBar3.Maximum - 200)
+                    progressBar3.Value += 200;
+                else progressBar3.Value = progressBar3.Maximum;
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e) //разбудить
+        {
+            if (life != 0)
+            {
+                pictureBox1.Image = imageList1.Images[0];
+                sleep = 0;
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if(progressBar1.Value<progressBar1.Maximum)
+                progressBar1.Value++; //голод
+            if (sleep == 1) //усталость
+            {
+                if (progressBar2.Value > progressBar2.Minimum + 10)
+                {
+                    progressBar2.Value -= 10;
+
+                }
+                else
+                {
+                    progressBar2.Value = progressBar2.Minimum; //просыпается сам когда усталость на нуле
+                    pictureBox1.Image = imageList1.Images[0];
+                    sleep = 0;
+                }
+            }
+            else {
+                if (progressBar2.Value < progressBar2.Maximum)
+                    progressBar2.Value++;
+            }
+            if(progressBar3.Value>progressBar3.Minimum)
+                progressBar3.Value--; //счастье
+            if(progressBar1.Value==progressBar1.Maximum && sleep == 0)
+                pictureBox1.Image = imageList1.Images[8];
+            if(progressBar2.Value==progressBar2.Maximum && sleep == 0)
+                pictureBox1.Image = imageList1.Images[4];
+            if(progressBar3.Value==progressBar3.Minimum && sleep == 0)
+                pictureBox1.Image = imageList1.Images[2];
+            if(progressBar1.Value == progressBar1.Maximum && progressBar2.Value == progressBar2.Maximum && progressBar3.Value == progressBar3.Minimum)
+            {
+                life = 0;
+                pictureBox1.Image = imageList1.Images[9];
+            }
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            if(sleep==0 && progressBar1.Value != progressBar1.Maximum && progressBar2.Value != progressBar2.Maximum && progressBar3.Value != progressBar3.Minimum && life!=0)
+                pictureBox1.Image = imageList1.Images[0]; //меняет спрайт на нейтральный после того как покормили/поиграли
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Image = imageList1.Images[0];
+            progressBar1.Value = 0;
+            progressBar2.Value = 0;
+            progressBar3.Value = 10000;
+            life = 1;
+        }
+
+        private void tamagochi_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
